2d74f0711bbcc77adde690f9ba7bfa437046cbfc
Chang, Liang <bigbrightf@gmail.com>
2013-10-11 17:01:31 +0800

    update 20131011.SimpleWindow

---
+++ WPF/20131011.SimpleWindow/20131011.SimpleWindow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace _20131011.SimpleWindow
+{
+   class Program
+   {
+      [STAThread]
+      static void Main(string[] args)
+      {
+         Window mywin = new Window();
+         mywin.Title = "Simple Window";
+         mywin.Content = "This is a simple window.";
+         mywin.Width = 200d;
+         mywin.Height = 100d;
+
+         Application myApp = new Application();
+         myApp.Run(mywin);
+      }
+   }
+}
