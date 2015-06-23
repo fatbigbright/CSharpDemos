1432f97e9a8d00a117465de4b3c3c7c25503afd1
fatbigbright <bigbrightf@gmail.com>
2015-06-23 16:55:04 +0800

    add MVVMDemo

---
+++ MVVMDemo/MVVMDemo/Model/ContentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVMDemo.Model
+{
+   public class ContentModel : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private string _text;
+      public string Text 
+      {
+         get
+         {
+            return _text;
+         }
+         set
+         {
+            _text = value;
+            if (PropertyChanged != null)
+            {
+               PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+            }
+         }
+      }
+   }
+}
---
+++ MVVMDemo/MVVMDemo/ViewModel/ContentViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVMDemo.Model;
+
+namespace MVVMDemo.ViewModel
+{
+   public class DelegateCommand : ICommand
+   {
+      public Action<object> ExecuteCommand = null;
+      public Func<object, bool> CanExecuteCommand = null;
+
+      public bool CanExecute(object parameter)
+      {
+         if (CanExecuteCommand != null)
+         {
+            return this.CanExecuteCommand(parameter);
+         }
+         else
+         {
+            return true;
+         }
+      }
+
+      public void Execute(object parameter)
+      {
+         if (ExecuteCommand != null)
+         {
+            ExecuteCommand(parameter);
+         }
+      }
+
+      public event EventHandler CanExecuteChanged;
+
+      public void RaiseCanExecuteChanged()
+      {
+         if (CanExecuteChanged != null) CanExecuteChanged(this, EventArgs.Empty);
+      }
+   }
+   public class ContentViewModel
+   {
+      public DelegateCommand ShowCommand { get; set; }
+      public DelegateCommand ClearCommand { get; set; }
+      public ContentModel Content { get; set; }
+      public ContentViewModel()
+      {
+         Content = new ContentModel();
+         ShowCommand = new DelegateCommand();
+         ShowCommand.ExecuteCommand = new Action<object>(obj => { 
+            Content.Text = "This is all what can be show to you!"; 
+         });
+
+         ClearCommand = new DelegateCommand();
+         ClearCommand.ExecuteCommand = new Action<object>(obj => { 
+            Content.Text = string.Empty; 
+         });
+      }
+   }
+}
